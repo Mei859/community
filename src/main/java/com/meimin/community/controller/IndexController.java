b5cc143dfe7aa9ee007249a1f453a15aba1dc0e4
meimin <1218217011@qq.com>
2020-01-03 11:06:30 +0800

    add bar

--- src/main/java/com/meimin/community/controller/IndexController.java
+++ src/main/java/com/meimin/community/controller/IndexController.java
@@ -9,12 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Controller是允许这个类去接收前端的请求
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        //返回值的作用，自动去resources的templates中找hello的模板
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
